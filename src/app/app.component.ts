b042ded9120647c9c64469d5370a12ca0dbd421a
Mehedi-Hassan <mehedikazi812@gmail.com>
2024-07-16 10:57:38 +0600

    Full project is ready
    -if need modify then can do it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe,FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Storing_and_retriving_UI';
+
+  http = inject(HttpClient);
+
+contactsForm = new FormGroup({
+  name: new FormControl<string>(''),
+  email: new FormControl<string | null>(null),
+  phone: new FormControl<string>(''),
+  favorite: new FormControl<boolean>(false)
+})
+
+submiteData(){
+    const addContactRequest = {
+      name:this.contactsForm.value.name,
+      email:this.contactsForm.value.email,
+      phone:this.contactsForm.value.phone,
+      favorite:this.contactsForm.value.favorite
+    }
+
+    this.http.post('https://localhost:7254/api/Contacts',addContactRequest)
+    .subscribe({
+      next:(value) => {
+        console.log(value)
+        this.contacts$ = this.getContacts();
+        this.contactsForm.reset();
+      }
+    });
+}
+
+ContactDelete(id: string){
+
+this.http.delete(`https://localhost:7254/api/Contacts/${id}`)
+    .subscribe({
+      next:(value) => {
+        alert('Item deleted');
+        this.contacts$ = this.getContacts();
+      }
+    })
+}
+
+contacts$ = this.getContacts();
+private getContacts(): Observable<Contact[]>{
+
+ return this.http.get<Contact[]>('https://localhost:7254/api/Contacts');
+
+}
+
 }
